8140c2520edd1104ea9a5e42197240ca56c4f3fb
Tess2019 <terese_burman@hotmail.com>
2020-03-26 19:59:59 +0000

    start to describe fizzbuzz functions

---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,15 @@
+describe ("my fizzBuzz function", function(){
+
+    beforeEach(function(){
+        fizzbuzz= new fizzBuzz();
+    } );
+
+describe("returns fizzbuzz, fizz, buzz or numbers", function(){
+    it ("should exist",function(){
+        expect (fizzBuzz).toBeDefined();
+    } );
+}
+
+
+
+}
