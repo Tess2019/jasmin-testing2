c39beb4448fee2ee5d3f87b0d82324e7fd11b86d
Tess2019 <terese_burman@hotmail.com>
2020-03-28 15:38:58 +0000

    start describe

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,13 +1,20 @@
-describe ("my fizzBuzz function", function(){
-
-    beforeEach(function(){
+describe ("my fizzbuzz game function", function(){
+it (should return numbers divisible by 3 and 5 );
+   
+beforeEach(function(){
         fizzbuzz= new fizzBuzz();
     } );
 
-describe("returns fizzbuzz, fizz, buzz or numbers", function(){
+describe("returns fizzBuzz, fizz, buzz or numbers", function(){
     it ("should exist",function(){
         expect (fizzBuzz).toBeDefined();
     } );
+
+    it("should return fizz when called as fizzBuzz (3)", function(){
+        var result = fizzBuzz(3);
+        expect(result).toBe(fizz);
+    });
+
 }
 
 
