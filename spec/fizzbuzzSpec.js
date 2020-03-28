a35a23d831cc4b3087bd4eb79fedabdf39a038e7
Tess2019 <terese_burman@hotmail.com>
2020-03-28 16:11:55 +0000

    adding more in describing game

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -14,6 +14,18 @@ describe("returns fizzBuzz, fizz, buzz or numbers", function(){
         var result = fizzBuzz(3);
         expect(result).toBe(fizz);
     });
+it("should return Buzz when called as fizzBuzz (5)", function(){
+        var result = fizzBuzz(5);
+        expect(result).toBe(Buzz);
+    });
+it("should return fizzBuzz when called as fizzBuzz (15)", function(){
+        var result = fizzBuzz(15);
+        expect(result).toBe(fizzBuzz);
+    });
+it("should return 0 or zero when called as fizzBuzz (0)", function(){
+        var result = fizzBuzz(0);
+        expect(result).toBe(Zero);
+    });
 
 }
 
