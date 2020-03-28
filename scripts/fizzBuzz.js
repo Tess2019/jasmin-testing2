13d25e9c95186a8cf5d17049a80815475ccd60cf
Tess2019 <terese_burman@hotmail.com>
2020-03-28 16:15:31 +0000

    change order and added else if

--- scripts/fizzBuzz.js
+++ scripts/fizzBuzz.js
@@ -1,12 +1,16 @@
 fizzBuzz = function(numbers){
-    if(number % 3===0){
+   if ( number % 3===0 && number % 5===0){
+        return "fizzBuzz";
+    }
+   
+    else if(number % 3===0){
         return  "fizz";
     }
 
-    if (number % 5===0){
-        return "Buzz"
+    else if (number % 5===0){
+        return "Buzz";
     }
-    if ( number % 3===0 && number % 5===0){
-        return "fizzBuzz"
+    else {
+        return number;
     }
 }
