9ec87153bf6ff0c29dfcb5baf504f32f0c28537e
Tess2019 <terese_burman@hotmail.com>
2020-03-29 09:14:04 +0000

    test if adding mor number in decsription works

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,25 +1,25 @@
 describe ("my fizzbuzz game function", function(){
 
-   
-beforeEach(function(){
+ beforeEach(function(){
         fizzbuzz= new fizzBuzz();
-    } );
+    } );  
 
 describe("returns fizzBuzz, fizz, buzz or numbers", function(){
     it ("should exist",function(){
         expect (fizzBuzz).toBeDefined();
-    } );
+    });
+    
 
-    it("should return fizz when called as fizzBuzz (3)", function(){
-        var result = fizzBuzz(3);
+    it("should return fizz when called as fizzBuzz (3,6,9)", function(){
+        var result = fizzBuzz(3,6,9);
         expect(result).toBe(fizz);
     });
-it("should return Buzz when called as fizzBuzz (5)", function(){
-        var result = fizzBuzz(5);
+it("should return Buzz when called as fizzBuzz (5,10)", function(){
+        var result = fizzBuzz(5,10);
         expect(result).toBe(Buzz);
     });
-it("should return fizzBuzz when called as fizzBuzz (15)", function(){
-        var result = fizzBuzz(15);
+it("should return fizzBuzz when called as fizzBuzz (15,30)", function(){
+        var result = fizzBuzz(15,30);
         expect(result).toBe(fizzBuzz);
     });
 it("should return 0 or Zero when called as fizzBuzz (0)", function(){
