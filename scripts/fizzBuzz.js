9b834c32b5964ef6d34669f620636b31e4649914
Tess2019 <terese_burman@hotmail.com>
2020-03-29 09:41:24 +0000

    deleted s from numbers and changed order

--- scripts/fizzBuzz.js
+++ scripts/fizzBuzz.js
@@ -1,17 +1,17 @@
-fizzBuzz = function(numbers){
+fizzBuzz = function(number){
 
-   if ( number % 3===0 && number % 5===0){
-        return "fizzBuzz";
-    }
-   
-    else if(number % 3===0){
+    if(number % 3===0){
         return  "fizz";
     }
 
     else if (number % 5===0){
         return "Buzz";
     }
+  else if ( number % 3===0 && number % 5===0){
+        return "fizzBuzz";
+    }
+
     else {
-        return number;
+        return number
     }
 };
