d8f270bdee24227b39e5f9177ef626a56b1f6b39
Tess2019 <terese_burman@hotmail.com>
2020-03-29 09:37:40 +0000

    alterd numbers and changed order

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -10,21 +10,21 @@ describe("returns fizzBuzz, fizz, buzz or numbers", function(){
     });
     
 
-    it("should return fizz when called as fizzBuzz (3,6,9)", function(){
-        var result = fizzBuzz(3,6,9);
+    it("should return fizz when called as fizzBuzz (3)", function(){
+        var result = fizzBuzz(3);
         expect(result).toBe(fizz);
     });
-it("should return Buzz when called as fizzBuzz (5,10)", function(){
-        var result = fizzBuzz(5,10);
+it("should return Buzz when called as fizzBuzz (5)", function(){
+        var result = fizzBuzz(5);
         expect(result).toBe(Buzz);
     });
-it("should return fizzBuzz when called as fizzBuzz (15,30)", function(){
-        var result = fizzBuzz(15,30);
+it("should return fizzBuzz when called as fizzBuzz (15)", function(){
+        var result = fizzBuzz(15);
         expect(result).toBe(fizzBuzz);
     });
-it("should return 0 or Zero when called as fizzBuzz (0)", function(){
-        var result = fizzBuzz(0);
-        expect(result).toBe(Zero);
+it("should return 2 when called as fizzBuzz (2)", function(){
+        var result = fizzBuzz(2);
+        expect(result).toBe(2);
     });
 
 });
